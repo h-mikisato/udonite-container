8282087b95048254f452001cc78942e7ec5eabe8
Hideyo Mikisato <h.mikisato@gmail.com>
2022-03-21 14:15:22 +0000

    とりあえずHTTPで動くようにする

---
+++ build-client.sh
@@ -0,0 +1,1 @@
+docker run --rm --mount type=bind,source=$PWD/public_html,target=/var/www/public_html udonite/udonite
---
+++ udonite-client/build.sh
@@ -0,0 +1,8 @@
+set -e
+
+npm run prod
+if [ -d /var/www/public_html/udonite ]; then
+  rm -r /var/www/public_html/udonite
+fi
+cd ./dist
+mv udonite /var/www/public_html
