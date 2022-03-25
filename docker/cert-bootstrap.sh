e585664755ad32f3b6d5fcf7b8d7ba485cd96d60
Hideyo Mikisato <h.mikisato@gmail.com>
2022-03-26 00:45:55 +0900

    バグ修正

--- docker/cert-bootstrap.sh
+++ docker/cert-bootstrap.sh
@@ -14,4 +14,4 @@ docker run --rm -it \
  --mount type=bind,source=$PWD/public_html,target=/var/www/public_html \
  --mount type=bind,source=$PWD/letsencrypt/etc_letsencrypt,target=/etc/letsencrypt \
  --mount type=bind,source=$PWD/letsencrypt/var_lib_letsencrypt,target=/var/lib/letsencrypt \
- certbot/certbot certonly certonly --webroot -w /var/www/public_html -d ${NGINX_HOST}
+ certbot/certbot certonly --webroot -w /var/www/public_html -d ${NGINX_HOST}
