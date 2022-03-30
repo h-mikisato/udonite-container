553f7652dc8c13d023c2615b0572c0ad5243f5c3
Hideyo Mikisato <h.mikisato@gmail.com>
2022-03-30 17:40:58 +0000

    kubernetes yamlを追加、configmap yamlを別途生成するように変更

--- podman/build-server.sh
+++ podman/build-server.sh
@@ -9,3 +9,11 @@ podman build \
   --build-arg GROUPID=$GROUPID \
   -t udonite/udonite-server ./udonite-server/
 podman build -t udonite/nginx-prod ./nginx-prod/
+
+if [ -f podman/host.yml ]; then
+  rm podman/host.yml
+fi
+
+. ./host.env
+cat podman/host.yml.template | sed -e s/__HOST_NAME__/${NGINX_HOST}/g > podman/host.yml
+chown $USERNAME. podman/host.yml
